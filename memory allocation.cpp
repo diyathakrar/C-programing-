fb98ff535f76c14e315de660d1c791828bb6922f
diyathakrar <diyathakrar381@gmail.com>
2025-07-11 22:07:07 +0530

    Create memory allocation.cpp

---
+++ memory allocation.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+	int* arr = new int[5];
+	for (int i=0; i<5; ++i) {
+		arr[i] = i + 1;
+	}
+	
+	for (int i = 0; i<5; ++i) {
+		std::cout << arr[i] <<"";
+	}
+	
+	std::cout << std::endl;
+	delete[] arr;
+	
+	return 0;
+}
